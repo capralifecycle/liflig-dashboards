f470195a1979cf92a085f35262a13829edfbcfd2
Jakob Lund Johannessen <jakob-lj@hotmail.com>
2023-11-03 16:15:48 +0100

    feat: add sops

--- scripts/chrome-startup.sh
+++ scripts/chrome-startup.sh
@@ -2,6 +2,7 @@
 
 CONFIG="$(curl https://raw.githubusercontent.com/capralifecycle/liflig-dashboards/master/configs/$(hostname).json)"
 
-URL=$(echo "$CONFIG" | jq -r '.website')
+
+URL=$(echo "$CONFIG" | sops --input-type json --output-type json -d /dev/stdin | jq -r '.website')
 
 chromium-browser "$URL"  --window-size=1922,1080 --window-position=0,0 --start-fullscreen  --noerrdialogs --disable-translate  --no-first-run  --fast  --fast-start  --disable-infobars  --disable-features=TranslateUI  --disk-cache-dir=/dev/null  --overscroll-history-navigation=0  --disable-pinch --hide-crash-restore-bubble
