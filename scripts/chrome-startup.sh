7b5c27e9dd261d6333754b533883c97593ab04c5
Kristian Rekstad <kre@capraconsulting.no>
2023-11-06 14:56:50 +0100

    Show IP and host as popup. Hide mouse cursor

--- scripts/chrome-startup.sh
+++ scripts/chrome-startup.sh
@@ -6,4 +6,10 @@ CONFIG="$(curl https://raw.githubusercontent.com/capralifecycle/liflig-dashboard
 
 URL=$(echo "$CONFIG" | sops --input-type json --output-type json -d /dev/stdin | jq -r '.website')
 
+
+notify-send --expire-time=0 --urgency=critical "$(hostname -I) $(hostname)"
+
+unclutter &
+
 chromium-browser "$URL"  --window-size=1922,1080 --window-position=0,0 --start-fullscreen  --noerrdialogs --disable-translate  --no-first-run  --fast  --fast-start  --disable-infobars  --disable-features=TranslateUI  --disk-cache-dir=/dev/null  --overscroll-history-navigation=0  --disable-pinch --hide-crash-restore-bubble
+
